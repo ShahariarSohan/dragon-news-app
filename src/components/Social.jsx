95b81ab1bc3ade85a218925d2635f4da09116bcd
Shahariar Sohan <Sohanshahariar4@gmail.com>
2025-04-19 16:49:22 +0600

    social section primary setup done

--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -1,9 +1,28 @@
 import React from "react";
+import { FaGoogle } from "react-icons/fa";
+import { FaFacebook, FaGithub, FaTwitter } from "react-icons/fa6";
 
 const Social = () => {
   return (
     <div>
       <h3 className="font-bold">Login With</h3>
+      <div className="space-y-5 my-5">
+        <button className="btn bg-slate-300 w-full space-x-2">
+          <FaGoogle></FaGoogle> Login with Facebook
+        </button>
+        <button className="btn bg-slate-300 w-full space-x-2">
+          <FaGithub></FaGithub> Login with Github
+        </button>
+      </div>
+      <h3 className="font-bold">Find Us On</h3>
+      <div className="space-y-5 mt-5">
+        <button className="btn bg-slate-300 w-full space-x-2">
+          <FaFacebook></FaFacebook> Login with Google
+        </button>
+        <button className="btn bg-slate-300 w-full space-x-2">
+          <FaTwitter></FaTwitter> Login with Twitter
+        </button>
+      </div>
     </div>
   );
 };
