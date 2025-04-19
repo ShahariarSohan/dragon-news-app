9320e637d3f1cf78342b544c2b2c277fc5700220
Shahariar Sohan <Sohanshahariar4@gmail.com>
2025-04-19 20:13:55 +0600

    login ,register page and auth provider added

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,47 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import { Link } from "react-router-dom";
 
 const Login = () => {
+  const handleLogin = (e) => {
+    e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    console.log(email, password);
+  };
   return (
     <div className="py-10">
       <Navbar></Navbar>
+      <div className="card bg-base-100 w-full max-w-lg mx-auto shrink-0 shadow-2xl py-5">
+        <form onSubmit={handleLogin} className="card-body">
+          <fieldset className="fieldset ">
+            <label className="label">Email</label>
+            <input
+              name="email"
+              type="email"
+              className="input w-full"
+              placeholder="Email"
+            />
+            <label className="label">Password</label>
+            <input
+              name="password"
+              type="password"
+              className="input w-full"
+              placeholder="Password"
+            />
+            <div>
+              <a className="link link-hover">Forgot password?</a>
+            </div>
+            <button className="btn btn-neutral mt-4">Login</button>
+          </fieldset>
+        </form>
+        <p className="text-center">
+          Don't have an account ?
+          <Link to="/register" className="ml-2 text-red-500">
+            Register
+          </Link>
+        </p>
+      </div>
     </div>
   );
 };
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,10 +1,65 @@
-import React from "react";
+import React, { useContext } from "react";
 import Navbar from "../components/Navbar";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../providers/AuthProvider";
 
 const Register = () => {
+  const authInfo = useContext(AuthContext);
+
+  const handleRegister = (e) => {
+    e.preventDefault();
+    const name = e.target.name.value;
+    const photoURL = e.target.photoURL.value;
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    console.log(name, photoURL, email, password);
+  };
   return (
     <div className="py-10">
       <Navbar></Navbar>
+      <div className="card bg-base-100 w-full max-w-lg mx-auto shrink-0 shadow-2xl py-5">
+        <form onSubmit={handleRegister} className="card-body">
+          <fieldset className="fieldset ">
+            <label className="label">Your Name</label>
+            <input
+              name="name"
+              type="text"
+              className="input w-full"
+              placeholder="name"
+            />
+            <label className="label">Photo URL</label>
+            <input
+              name="photoURL"
+              type="text"
+              className="input w-full"
+              placeholder="Photo URL"
+            />
+            <label className="label">Email</label>
+            <input
+              name="email"
+              type="email"
+              className="input w-full"
+              placeholder="Email"
+            />
+            <label className="label">Password</label>
+            <input
+              name="password"
+              type="password"
+              className="input w-full"
+              placeholder="Password"
+            />
+            <button type="submit" className="btn btn-neutral mt-4">
+              Register
+            </button>
+          </fieldset>
+        </form>
+        <p className="text-center">
+          Already have an account ?
+          <Link to="/login" className="ml-2 text-red-500">
+            Login
+          </Link>
+        </p>
+      </div>
     </div>
   );
 };
